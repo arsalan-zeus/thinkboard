834da39359f2761069c5e2135616533d3f32cc4f
arsalan-zeus <prince.arsalang123@gmail.com>
2025-08-02 20:28:16 +0530

    backend:mailtrap: add email sending functionality.

---
+++ backend/src/mailtrap/emails.js
@@ -0,0 +1,20 @@
+import { mailtrapClient, sender } from "./mailtrap.config.js";
+import { VERIFICATION_EMAIL_TEMPLATE, PASSWORD_RESET_REQUEST_TEMPLATE, PASSWORD_RESET_SUCCESS_TEMPLATE } from "./emailTemplates.js";
+
+export const sendVerificationEmail = async (email, verificationToken) => {
+    const recipient = [{email}];
+
+    try {
+        const response = await mailtrapClient.send({
+            from: sender,
+            to: recipient,
+            subject: "Verify Your Email",
+            html: VERIFICATION_EMAIL_TEMPLATE.replace("{verificationCode}", verificationToken),
+            category: "Email Verification"
+        });
+        console.log("Verification email sent successfully:", response);
+    } catch (error) {
+        console.error("Error sending verification email:", error);
+        throw new Error(`Failed to send verification email: ${error}`);
+    }
+};
