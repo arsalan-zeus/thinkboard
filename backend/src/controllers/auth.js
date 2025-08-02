8b0e508de3652ea8d3a0d86b62752a80524dcdf2
arsalan-zeus <prince.arsalang123@gmail.com>
2025-08-02 17:09:02 +0530

    backend:controllers: add signup logic with token generation and cookie setting.

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -1,11 +1,59 @@
+import { User } from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
+
 export const signup = async (req, res) => {
-    res.send("Signup Route");
+  const { email, password, name } = req.body;
+  try {
+    if (!email || !password || !name) {
+      return res.status(400).json({ success: false, message: "All fields are required" });
+    }
+    const userAlreadyExists = await User.findOne({ email });
+    if (userAlreadyExists) {
+      return res.status(409).json({ success: false, message: "User already exists" });
+    }
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    const verificationToken = Math.floor(
+      100000 + Math.random() * 900000
+    ).toString();
+    const user = new User({
+      email,
+      password: hashedPassword,
+      name,
+      verificationToken,
+      verificationTokenExpiresAt: Date.now() + 30 * 60 * 1000,
+    }); // Token valid for 30 minutes
+
+    await user.save();
+
+    generateTokenAndSetCookie(res, user._id);
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: {
+        ...user._doc,
+        password: undefined, // Exclude password from response
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
 };
 
 export const login = async (req, res) => {
-    res.send("Login Route");
+  try {
+    // Logic for user login
+    res.status(200).send("Login Route");
+  } catch (error) {
+    res.status(500).send("Internal Server Error");
+  }
 };
 
 export const logout = async (req, res) => {
-    res.send("Logout Route");
+  try {
+    // Logic for user logout
+    res.status(200).send("Logout Route");
+  } catch (error) {
+    res.status(500).send("Internal Server Error");
+  }
 };
