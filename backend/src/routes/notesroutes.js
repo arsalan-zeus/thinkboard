dbc2f1ab00119884a93deb6cf4244b3ffb0c5e4a
arsalan-zeus <prince.arsalang123@gmail.com>
2025-08-08 21:57:54 +0530

    feat(backend): Protect note creation route with token verification

--- backend/src/routes/notesroutes.js
+++ backend/src/routes/notesroutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { getAllNotes, getNoteById, createNote, updateNote, deleteNote } from '../controllers/notesController.js';
+import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
 router.get("/", getAllNotes);
 router.get("/:id", getNoteById);
-router.post("/", createNote);
+router.post("/", verifyToken, createNote);
 router.put("/:id", updateNote);
 router.delete("/:id", deleteNote);
 
